2c6d3f61a3445d0b319e9af38b5ebde32957786b
Paolo Viotti <paolo.viotti@lip6.fr>
2018-01-13 18:02:34 +0100

    add rest api scaffolding

---
+++ api.md
@@ -0,0 +1,13 @@
+# Antidote web shell API
+
+POST /api/:rep_id/set              set_name
+    create a set
+
+GET /api/:rep_id/set/:set_id
+    read a set
+    
+PUT /api/:rep_id/set/:set_id     value
+    add an element to a set
+    
+DELETE /api/:rep_id/set/:set_id     value
+    remove an element from a set
--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var app = express();
-var router = express.Router();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -18,13 +17,50 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 var viewPath = __dirname + '/views/';
 
-/* GET home page. */
+/* Static web page routing. */
+var router = express.Router();
 router.get('/', function(req, res, next) {
   res.sendFile(viewPath + 'index.html');
 });
+app.use("/", router);
 
-app.use("/",router);
+/* API routing. */
+var apiRouter = express.Router();
+apiRouter.get('/', function(req, res, next) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+apiRouter.route('/:rep_id/set')
+    .post(function(req, res) {
+        var rep_id = req.params.rep_id;  
+        var set_id = req.body.set_id;  
+        console.log('Created', set_id, 'on replica', rep_id)
+        res.json({ message: 'Created '+ set_id + ' on replica ' + rep_id });
+    });
+apiRouter.route('/:rep_id/set/:set_id')
+    .get(function(req, res) {
+        var rep_id = req.params.rep_id;  
+        var set_id = req.params.set_id;  
+        console.log('Get', set_id, 'from replica', rep_id)
+        res.json({ message: 'Get '+ set_id + ' from replica ' + rep_id });
+    })
+    .put(function(req, res) {
+        var rep_id = req.params.rep_id;  
+        var set_id = req.params.set_id;  
+        var value = req.body.value;
+        console.log('Add', value, 'to', set_id, 'on replica', rep_id)
+        res.json({ message: 'Add '+ value+ ' to '+ set_id+ ' on replica '+ rep_id });
+    })
+    .delete(function(req, res) {
+        var rep_id = req.params.rep_id;  
+        var set_id = req.params.set_id; 
+        var value = req.body.value; 
+        console.log('Remove', value, 'from', set_id, 'on replica', rep_id)
+        res.json({ message: 'Remove '+ value+ ' from '+ set_id+ ' on replica '+ rep_id });
+    });
 
+app.use("/api", apiRouter);
+
+/* Default routing. */
 app.use("*",function(req,res){
   res.sendFile(viewPath + "404.html");
 });
