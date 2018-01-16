e168a5e7eebbe575d8597c320525f0cb654d798c
Paolo Viotti <paolo.viotti@lip6.fr>
2018-01-16 18:45:51 +0100

    favicon

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
