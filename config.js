8f7fb8c627d09bf0db96a78c0d5070c8f347eeb8
Paolo Viotti <paolo.viotti@lip6.fr>
2018-01-30 18:22:40 +0100

    add config file

---
+++ config.js
@@ -0,0 +1,19 @@
+const config = {
+    antidote: [
+        {
+            host: 'localhost',
+            port: 8087
+        },
+        {
+            host: 'localhost',
+            port: 8088
+        },
+        {
+            host: 'localhost',
+            port: 8089
+        }
+    ]
+};
+
+module.exports = config;
+
