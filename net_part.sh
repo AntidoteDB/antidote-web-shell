f4c8a006a4df6041e89a0cb433fc22b2e933b74f
Paolo Viotti <paolo.viotti@lip6.fr>
2018-02-02 10:24:01 +0100

    docker image based on Alpine Linux and including iptables

--- net_part.sh
+++ net_part.sh
@@ -6,16 +6,15 @@ REPLICA_NAME=antidote
 
 case "$1" in
 ispart)
-    docker-compose -f $DOCKER_FILE exec --privileged $REPLICA_NAME$2 bash -c \
-        '(iptables -V || (apt update && apt install -y iptables)) && iptables -L -n -v' | grep DROP
+    docker-compose -f $DOCKER_FILE exec --privileged $REPLICA_NAME$2 bash -c 'iptables -L -n -v' | grep DROP
     ;;
 create)
     docker-compose -f $DOCKER_FILE exec --privileged $REPLICA_NAME$2 bash -c \
-        '(iptables -V || (apt update && apt install -y iptables)) && (iptables -A INPUT -p tcp --dport 8086 -j DROP; iptables -A OUTPUT -p tcp --dport 8086 -j DROP)'
+        'iptables -A INPUT -p tcp --dport 8086 -j DROP; iptables -A OUTPUT -p tcp --dport 8086 -j DROP'
     ;;
 remove)
     docker-compose -f $DOCKER_FILE exec --privileged $REPLICA_NAME$2 bash -c \
-        '(iptables -V || (apt update && apt install -y iptables)) && iptables -D INPUT -p tcp --dport 8086 -j DROP; iptables -D OUTPUT -p tcp --dport 8086 -j DROP'
+        'iptables -D INPUT -p tcp --dport 8086 -j DROP; iptables -D OUTPUT -p tcp --dport 8086 -j DROP'
     ;;
 *)
     echo $"Usage: $0 {create|remove|ispart}"
